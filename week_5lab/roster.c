5c105b8ab50a67210e8245b28c94780e7a61b51d
Jeronimo Leon <jleon5664@gmail.com>
2026-06-19 16:54:32 +0000

    gah

--- week_5lab/roster.c
+++ week_5lab/roster.c
@@ -28,4 +28,33 @@ Student create_student(const char *first, const char *last, int id, double gpa) 
     return temp;
 }
 
+const char *grade_to_string(Grade g) {
+    switch(g) {
+        case GRADE_A: return "A";
+        case GRADE_B: return "B";
+        case GRADE_C: return "C";
+        case GRADE_D: return "D";
+        case GRADE_F: return "F";
+        default:   return "Incomplete";
+    }
+}
 
+void print_student(const Student *s) {
+    if (s == 0) {
+        printf("No Student to Print\n");
+        return;
+    }
+    // -> operator to reach within pointer to print
+    printf("[%i] %s, %s \t GPA: %.2f \t Standing: %s\n",
+            s->student_id, s->last_name, s->first_name, s->gpa, grade_to_string(s->standing));
+}
+
+void print_roster(const Roster *r) {
+    printf("Student Roster (%i students)\n", r->count);
+
+    // loops through active count 
+    for (int i = 0; i < r->count; i++) {
+        // pass add of student at i 
+        print_student(&(r->students[i]));
+    }
+}
