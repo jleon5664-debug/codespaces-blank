f2d4d4cf7cf2ca47fe2f79e16338f46607cd933f
Jeronimo Leon <jleon5664@gmail.com>
2026-06-19 21:27:34 +0000

    phase 2 roster

--- week_5lab/main.c
+++ week_5lab/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
   #include "roster.h"
 
-int main() {
+int main(void) {
+
+    Roster my_roster;
+    my_roster.count = 0;
+
+    char input_first[32];
+    char input_last[32];
+    int input_id;
+    double input_gpa; 
+
     int choice = 0; 
-        // math menu functions  
-        while (choice != 8) {
+        // roster menu options 
+        do {
             printf("= Student Roster System =\n");
             printf("1. Add student\n");
             printf("2. Remove student (by ID)\n");
@@ -14,34 +23,54 @@ int main() {
             printf("6. Sort by GPA\n");
             printf("7. Print roster\n");
             printf("8. Quit\n");   
+            printf("Enter choice (1-8): \n");
         
-        scanf ("%i", &choice);
+        if (scanf("%i", &choice) != 1) {
+            // clears letter input from memory
+            while (getchar() != '\n');
+            printf("Invalid selection. Please enter 1-8.\n");
+            continue;
+        }
 
     switch (choice) {
-            case 1: {
-                char first[32], last[32];
-                int id;
-                double gpa;
-
+        case 1: {
                 printf("First name: \n");
                 // limit scanf to 31 characters
-                scanf("%31s", first);
-                printf("Last name: \n");
-                scanf("%31s", last);
-                printf("Student ID: \n");
-                scanf("%i", &id);
-                printf("GPA: \n");
-                scanf("%lf", &gpa);
+                scanf("%31s", input_first);
 
+                printf("Last name: \n");
+                scanf("%31s", input_last);
+
+                printf("Student ID: \n");
+                scanf("%i", &input_id);
+
+                printf("GPA: \n");
+                scanf("%lf", &input_gpa);
+            
+            Student new_student = create_student(input_first, input_last, input_id, input_gpa);
+
+            int result = roster_add(&my_roster, new_student);
+
+            if (result == 1) {
+                printf("Student added.\n");
+            } else if (result == 0) {
+                printf("Roster Full, Student Not Added\n");
+            } else if (result == -1) {
+                printf("ID Already Exists, Student Not Added\n");
             }
-            case 8: {
-                printf("Goodbye!\n");
-                break;
-            }
-    default: {
-            printf("Invalid choice. Please choose 1-8\n");
             break;
-    }
-    }
-        return 0;
         }
+        case 7:{ 
+            printf("\n");
+            print_roster(&my_roster);
+            break;
+        } 
+        case 8: {
+            printf("Goodbye!\n");
+            break;
+        }
+    }
+} while (choice !=8);
+
+    return 0;
+}
--- week_5lab/roster.c
+++ week_5lab/roster.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "roster.h"
 
+// create student
 Student create_student(const char *first, const char *last, int id, double gpa) {
     Student temp;
 
@@ -39,22 +40,99 @@ const char *grade_to_string(Grade g) {
     }
 }
 
+// print student logic
 void print_student(const Student *s) {
     if (s == 0) {
         printf("No Student to Print\n");
         return;
     }
-    // -> operator to reach within pointer to print
-    printf("[%i] %s, %s \t GPA: %.2f \t Standing: %s\n",
-            s->student_id, s->last_name, s->first_name, s->gpa, grade_to_string(s->standing));
+    // combine first & last into single string full_name
+    char full_name[66];
+    snprintf(full_name, sizeof(full_name), "%s, %s", s->last_name, s->first_name);
+
+    // -> operator reach within pointer & print w no gaps
+    printf("[%i] %-24s GPA: %.2f Standing: %s\n",
+            s->student_id, full_name, s->gpa, grade_to_string(s->standing));
 }
 
+// print roster logic 
 void print_roster(const Roster *r) {
-    printf("Student Roster (%i students)\n", r->count);
+
+        printf("==========================================================\n");
+        printf("Student Roster (%i students)\n", r->count);
+        printf("==========================================================\n");
 
     // loops through active count 
     for (int i = 0; i < r->count; i++) {
         // pass add of student at i 
         print_student(&(r->students[i]));
     }
+        printf("==========================================================\n");
+        printf("Class average GPA: %.2f\n", roster_average_gpa(r));
+        printf("==========================================================\n");
+    
+}
+
+int roster_add(Roster *r, Student s) {
+    // check if array is maxed
+    if (r->count >= MAX_STUDENTS) {
+        return 0;
+    }
+    
+    // check for duplicate ID
+    for (int i = 0; i < r->count; i++) {
+        if (r->students[i].student_id == s.student_id) {
+            return -1;
+        }
+    }
+    
+    // add student struct block to active pool
+    r->students[r->count] = s;
+
+    // increment to move student data to next block
+    r->count++;
+
+    return 1;
+}
+
+double roster_average_gpa(const Roster *r) {
+    // prevent div by zero
+    if (r == 0 || r->count == 0) {
+        return 0.0;
+    }
+    
+        double total_gpa = 0.0;
+
+    // acummulate gpa
+        for (int i = 0; i < r->count; i++) {
+            total_gpa += r->students[i].gpa;
+        }
+        
+    // return average
+        return total_gpa / r->count;
+}
+
+int roster_remove(Roster *r, int student_id) {
+    int target_index = -1;
+
+    // search array
+    for (int i = 0; i < r->count; i++) {
+        if (r->students[i].student_id = student_id) {
+            target_index = i;
+            break;
+        }
+    }
+    // if student doesn't exist
+    if (target_index == -1) {
+        return 0;
+    }
+    // compaction
+    for (int d = target_index; d < r->count - 1; d++) {
+        // shifts left 
+        r->students[d] = r->students[d + 1];
+    }
+    // update status
+        r->count--;
+
+    return 1;
 }
