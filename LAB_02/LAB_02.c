ba3996d68f938afbf401813ab018ee14c2cfcf6b
Jeronimo Leon <jleon5664@gmail.com>
2026-09-09 00:04:20 +0000

    lab 2 recursion

---
+++ LAB_02/LAB_02.c
@@ -0,0 +1,77 @@
+// rescursion 
+// number system = decimal system
+// 0 - 9 
+// calc base, use base to power of position
+// positional numbering system
+
+
+// binary system - base is on lower right
+// (1 * 2^0) + (0 + 2^1)
+// past value 9, letters are used
+
+
+// octal number system 
+// base is 8 
+// (7 * 8^0) + (5 * 8^1) + (0 * 8^2) + (2 * 8^3) = 1071
+// 
+
+
+
+// hexadecimal number system 
+// base of 16
+// 
+
+//1110101_2  = 117_10
+// base^power of position
+// (1 * 2^0) + (0 * 2^1) + (1 * 2^2)...
+
+// 117_10 conversion use long division
+// 117/2 ->  A - 58 and R - 1
+// 58/2 - 29 - 1
+// 29/2 - 12 - 1
+// 14/2 - 14 - 0
+// 7/2 - 7 - 1
+// 3/2 - - 0
+// 1/2 - - 1
+
+// hexidecimal and octal shortcut
+// split into 3's 
+// 10110_2 = 26^8
+// 101110
+
+
+// 562_8 octal to binary conv
+// 101 110 010
+// 101110010
+
+
+// 175_10 
+// 175/3 - 58 - 1
+// 58/3 - 19 - 1
+// 19/3 - 6 - 1
+// 6/3 - 2 - 0
+// 2/3 - 1 - 2
+// 0/3 - 0 - 0
+// bottom to top 20111_3
+
+// binary to hexidecimal 
+// 11010011_2 = ?_16
+// 1101 - D
+// 0011 - 3
+
+// 3AD_16 = ?_2
+// 0011 1010 1101
+
+// ABC_16 = ?_2
+// 1010 1011 1100
+
+// (381)_10 = (?)_7
+// 381/7 - 54 - 3
+// 54/7 - 7 - 5
+// 7/7 - 1 - 0
+// 1/7 - 0 - 6
+// 1053
+
+// (ABC)_16 = (?)_8
+// 1010 1011 1100
+// 4725
